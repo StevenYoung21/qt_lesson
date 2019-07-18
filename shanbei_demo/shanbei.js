4472d00a4135bf6158b14e10835d8ae656fe80bb
StevenYoung21 <36201784+StevenYoung21@users.noreply.github.com>
2019-07-18 17:54:23 +0800

    仿写扇贝首页

---
+++ shanbei_demo/shanbei.js
@@ -0,0 +1,16 @@
+function changeInputStyle(){
+var inputBtnStyle = document.getElementById('input__btn');
+inputBtnStyle.style.cssText = "width:180px;background-color:white;";
+
+var inputStyle = document.getElementById('input');
+inputStyle.style.cssText = "width:150px;background-color:white;color:#666;";
+}
+
+function initailStyle(){
+    var inputBtnStyle = document.getElementById('input__btn');
+    inputBtnStyle.style.cssText = "width:97px;background-color:#209E85;";
+    
+    var inputStyle = document.getElementById('input');
+    inputStyle.style.cssText = "width:71px;background-color::#209E85;color:white;";
+}
+ 
