6df7ac59d5f7eb9a484e508cee5d06488181c05d
StevenYoung21 <719192406@qq.com>
2019-07-24 15:13:17 +0800

    大数据处理

---
+++ LeetCode/coco_bananas/test.js
@@ -0,0 +1,19 @@
+// Math.max()
+const IMath = {}  //namespace
+/**
+ * @params:number NaN
+ * @return:Number 返回最大值
+ *  
+ */
+IMath.max = function(...args){
+    // console.log(arguments, arguments.length);
+    for ( var i = 0; i < args.length; i++){
+        // 类型检测
+        //  console.log(arguments[i])
+        if(typeof args[i] !== 'number'){
+            return NaN;
+        }
+    }
+}
+
+console.log(IMath.max(...[2, 4, 3, 9]));
---
+++ LeetCode/lru/index.js
@@ -0,0 +1,23 @@
+/** 
+ * 最近最少使用原则
+ * @param capacity:number 容量
+ * 
+*/
+
+ var LRUCache = function(capacity) {
+    this.capacity = capacity;  //空间的上限
+    this.arr = []; //数据结构
+    this.obj = {}; // 3 , 3
+};
+
+LRUCache.prototype.get = function (key){
+    // 调整优先级
+    
+    return ;
+}
+
+LRUCache.prototype.set = function(){
+
+}
+new LRUCache(2);
+
